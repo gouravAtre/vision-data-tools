7ec4fef64589a17ac0f9f26b70fa7ebd35fa509a
Gourav Atre <gouravkumar1815@gmail.com>
2023-05-30 18:47:22 +0530

    feat: escape and undo functionality added

--- src/movefiles_by_keys.py
+++ src/movefiles_by_keys.py
@@ -22,14 +22,14 @@ Process brief:
 
 import os
 import cv2
-import time
+import numpy as np
 
 
 def main(folder_path):
     # Define the folder path where the images are located
     files = os.listdir(folder_path)
     files = [f for f in files if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.jpeg')]
-
+    total_n_files = len(files)
     last_file = {"filepath":"",
                  "folder_name": ""}
     # Iterate over all the files in the folder
@@ -38,6 +38,16 @@ def main(folder_path):
         img_path = os.path.join(folder_path, file_name)
         img = cv2.imread(img_path)
 
+        # print(img.shape)
+        tx, ty = 10, 20
+        border_thickness = 1
+        thickness=1
+        font = cv2.FONT_HERSHEY_DUPLEX
+        font_sz = 0.5
+        
+        # For weighted img fullness
+        cv2.putText(img, f"{total_n_files-ind}", (tx - border_thickness, ty - border_thickness), font, font_sz, (0, 0, 0), thickness+border_thickness, cv2.LINE_AA)
+        cv2.putText(img, f"{total_n_files-ind}", (tx, ty), font, font_sz, (255, 255, 255), thickness)
         # Show the image
         window_name = file_name
         cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
@@ -45,34 +55,64 @@ def main(folder_path):
         cv2.imshow(window_name, img)
         key = cv2.waitKey()
         
+        # Close the window if 'Esc' key is pressed
+        if key == 27:
+            break
 
         if str(chr(key)) == "z":
-            # Show the image
-            undo_image = cv2.imread(last_file["filepath"])
-            window_name = "Undo window"
-            cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-            cv2.resizeWindow(window_name, 700, 700)
-            cv2.imshow(window_name, undo_image)
-            key = cv2.waitKey()
-            # try:
-            # Take text input
-            new_folder_name = str(chr(key))
-            last_dirname = os.path.dirname(last_file["filepath"])
-            last_dirname_replacement = last_dirname.rstrip("/")[:-1] + new_folder_name
-            undo_filename = last_file["filepath"].replace(last_dirname, last_dirname_replacement)
+            if last_file["filepath"] == "":
+                # Create a blank image with white background
+                image = 255 * np.ones((700, 700, 3), dtype=np.uint8)
 
-            print(f'Undo :: "{undo_filename}" : "{new_folder_name}"')
+                # Write the text on the image
+                cv2.putText(image, "Can't undo on first image!", (10, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+                cv2.putText(image, "Press any key to continue...", (10, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+                # Display the image
+                cv2.imshow("Blank Image", image)
+                cv2.waitKey(0)
+                cv2.destroyWindow(window_name)
+                
+                # Show the image
+                window_name = file_name
+                cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+                cv2.resizeWindow(window_name, 700, 700)
+                cv2.imshow(file_name, img)
+                key = cv2.waitKey()
+                # Close the window if 'Esc' key is pressed
+                if key == 27:
+                    break
 
-            os.makedirs(os.path.dirname(undo_filename), exist_ok=True)
-            os.rename(last_file["filepath"], undo_filename)
-            cv2.destroyWindow(window_name)
+            else:
+                # Show the image
+                undo_image = cv2.imread(last_file["filepath"])
+                window_name = "Undo window"
+                cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+                cv2.resizeWindow(window_name, 700, 700)
+                cv2.imshow(window_name, undo_image)
+                key = cv2.waitKey()
+                # try:
+                # Take text input
+                new_folder_name = str(chr(key))
+                last_dirname = os.path.dirname(last_file["filepath"])
+                last_dirname_replacement = last_dirname.rstrip("/")[:-1] + new_folder_name
+                undo_filename = last_file["filepath"].replace(last_dirname, last_dirname_replacement)
 
-            # Show the image
-            window_name = file_name
-            cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-            cv2.resizeWindow(window_name, 700, 700)
-            cv2.imshow(file_name, img)
-            key = cv2.waitKey()
+                print(f'Undo :: "{undo_filename}" : "{new_folder_name}"')
+
+                os.makedirs(os.path.dirname(undo_filename), exist_ok=True)
+                os.rename(last_file["filepath"], undo_filename)
+                cv2.destroyWindow(window_name)
+                
+                                                                            
+                # Show the image
+                window_name = file_name
+                cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+                cv2.resizeWindow(window_name, 700, 700)
+                cv2.imshow(file_name, img)
+                key = cv2.waitKey()
+                # Close the window if 'Esc' key is pressed
+                if key == 27:
+                    break
 
         try:
             # Take text input
@@ -84,6 +124,7 @@ def main(folder_path):
 
             if not os.path.exists(new_folder_path):
                 os.makedirs(new_folder_path)
+
             new_img_path = os.path.join(new_folder_path, file_name)
             os.rename(img_path, new_img_path)
 
@@ -96,5 +137,5 @@ def main(folder_path):
 
 
 if __name__ == "__main__":
-    folder_path = "/home/gourav/Downloads/disha_tray_parts_task1"
+    folder_path = "enter the folder path here"
     main(folder_path)
